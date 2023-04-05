c191c370eab926e6f1f84b2a186071795ca82e75
Dev <usman@yourparsley.com>
2023-04-05 06:42:56 +0500

    Initial coomit

---
+++ chatgpt-in-rails-cli/lib/chatgpt/in/rails/cli.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "cli/version"
+
+module Chatgpt
+  module In
+    module Rails
+      module Cli
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ chatgpt-in-rails-cli/lib/chatgpt/in/rails/cli/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Chatgpt
+  module In
+    module Rails
+      module Cli
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
